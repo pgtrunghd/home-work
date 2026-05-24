0445d95f20251722a0d23973f8a8d1021d87d883
pgtrunghd <80527217+pgtrunghd@users.noreply.github.com>
2026-05-25 01:26:39 +0700

    Add files via upload

---
+++ session3_bai6.sql
@@ -0,0 +1,18 @@
+-- CREATE DATABASE LibraryDB;
+
+CREATE SCHEMA library;
+
+CREATE TABLE library.Books (
+	book_id SERIAL PRIMARY KEY,
+	title VARCHAR(100) NOT NULL,
+	author VARCHAR(50) NOT NULL,
+	published_year INT NOT NULL,
+	available BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE library.Members (
+	member_id SERIAL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	email VARCHAR(100) NOT NULL UNIQUE,
+	join_date DATE DEFAULT CURRENT_DATE
+);
